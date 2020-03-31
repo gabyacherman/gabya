0c1778eb8d3a9fd0c706d3f8217e18a2e0ad2354
Gaby Acherman <you@example.com>
2020-03-31 15:37:42 -0400

    debug 5

---
+++ Web Lab/homework/JQuery/JQueryA.js
@@ -0,0 +1,116 @@
+$(document).ready(function(){
+
+  $("main .1").hide();
+  $("main .2").hide();
+  $("main .3").hide();
+  $("main .4").hide();
+  $("main .5").hide();
+  $("main .6").hide();
+  $("main .7").hide();
+  $("main .8").hide();
+  $("main .9").hide();
+
+
+  $("aside ul li:first-child").click(function(){
+    $("main .1").show();
+    $("main .2").hide();
+    $("main .3").hide();
+    $("main .4").hide();
+    $("main .5").hide();
+    $("main .6").hide();
+    $("main .7").hide();
+    $("main .8").hide();
+    $("main .9").hide();
+  });
+
+  $("aside ul li:nth-child(2)").click(function(){
+    $("main .1").hide();
+    $("main .2").show();
+    $("main .3").hide();
+    $("main .4").hide();
+    $("main .5").hide();
+    $("main .6").hide();
+    $("main .7").hide();
+    $("main .8").hide();
+    $("main .9").hide();
+  });
+
+  $("aside ul li:nth-child(3)").click(function(){
+    $("main .1").hide();
+    $("main .2").hide();
+    $("main .3").show();
+    $("main .4").hide();
+    $("main .5").hide();
+    $("main .6").hide();
+    $("main .7").hide();
+    $("main .8").hide();
+    $("main .9").hide();
+  });
+  $("aside ul li:nth-child(4)").click(function(){
+    $("main .1").hide();
+    $("main .2").hide();
+    $("main .3").hide();
+    $("main .4").show();
+    $("main .5").hide();
+    $("main .6").hide();
+    $("main .7").hide();
+    $("main .8").hide();
+    $("main .9").hide();
+  });
+  $("aside ul li:nth-child(5)").click(function(){
+    $("main .1").hide();
+    $("main .2").hide();
+    $("main .3").hide();
+    $("main .4").hide();
+    $("main .5").show();
+    $("main .6").hide();
+    $("main .7").hide();
+    $("main .8").hide();
+    $("main .9").hide();
+  });
+  $("aside ul li:nth-child(6)").click(function(){
+    $("main .1").hide();
+    $("main .2").hide();
+    $("main .3").hide();
+    $("main .4").hide();
+    $("main .5").hide();
+    $("main .6").show();
+    $("main .7").hide();
+    $("main .8").hide();
+    $("main .9").hide();
+  });
+  $("aside ul li:nth-child(7)").click(function(){
+    $("main .1").hide();
+    $("main .2").hide();
+    $("main .3").hide();
+    $("main .4").hide();
+    $("main .5").hide();
+    $("main .6").hide();
+    $("main .7").show();
+    $("main .8").hide();
+    $("main .9").hide();
+  });
+  $("aside ul li:nth-child(8)").click(function(){
+    $("main .1").hide();
+    $("main .2").hide();
+    $("main .3").hide();
+    $("main .4").hide();
+    $("main .5").hide();
+    $("main .6").hide();
+    $("main .7").hide();
+    $("main .8").show();
+    $("main .9").hide();
+  });
+  $("aside ul li:nth-child(9)").click(function(){
+    $("main .1").hide();
+    $("main .2").hide();
+    $("main .3").hide();
+    $("main .4").hide();
+    $("main .5").hide();
+    $("main .6").hide();
+    $("main .7").hide();
+    $("main .8").hide();
+    $("main .9").show();
+  });
+
+  });
